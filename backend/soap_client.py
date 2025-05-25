99e8d95a7d0afb2699ceddaba6e909e362fd7970
bercho <you@example.comberprado@gmail.com>
2025-05-25 03:33:49 -0400

    3

--- backend/soap_client.py
+++ backend/soap_client.py
@@ -1,12 +1,26 @@
 ﻿from zeep import Client
+from zeep.transports import Transport
 from config import Settings
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 settings = Settings()
 
-client = Client(wsdl=settings.WSDL_URL)
+session = requests.Session()
+session.headers.update({
+    "apikey": settings.API_KEY
+})
+
+client = Client(wsdl=settings.WSDL_URL, transport=Transport(session=session))
 
 def verificar_comunicacion():
     response = client.service.verificarComunicacion()
-    return {"resultado": response}
+    # Extraer descripción del mensaje si existe
+    try:
+        return {
+            "descripcion": response['mensajesList'][0]['descripcion'],
+            "transaccion": response['transaccion']
+        }
+    except Exception:
+        return {"error": "Respuesta inesperada", "raw": response}
